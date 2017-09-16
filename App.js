2b7c74ac898d030e080fc6eb2d6c28ac411e57d7
Jordan Hart <jordanhart@berkeley.edu>
2017-09-16 13:12:28 -0700

    5 categories for app now

--- App.js
+++ App.js
@@ -44,22 +44,22 @@ export default class CardImageExample extends Component {
                 </Button>
               </Body>
               <Right>
-                <Text>11h ago</Text>
+                <Text>Points earned: 1024</Text>
               </Right>
             </CardItem>
           </Card>
           <Card>
             <CardItem>
               <Left>
-                <Thumbnail source={{uri: 'https://genesistransformation.files.wordpress.com/2014/11/coffee.jpg'}} />
+                <Thumbnail source={{uri: 'http://www.fashionfromusa.com/images/slider1.jpg'}} />
                 <Body>
-                  <Text>Dessert</Text>
-                  <Text note>Icecream shops, donut shops, etc</Text>
+                  <Text>Clothes</Text>
+                  <Text note>Forever 21, H&M, Zara, Nordstrom, URban Outfitters</Text>
                 </Body>
               </Left>
             </CardItem>
             <CardItem cardBody>
-              <Image source={{uri: 'http://images.girlslife.com/image.axd?i=91125'}} style={{height: 200, width: null, flex: 1}}/>
+              <Image source={{uri: 'http://retaildesignblog.net/wp-content/uploads/2012/11/VILA-Clothes-shop-by-Riis-Retail-Copenhagen-02.jpg'}} style={{height: 200, width: null, flex: 1}}/>
             </CardItem>
             <CardItem>
               <Left>
@@ -71,11 +71,42 @@ export default class CardImageExample extends Component {
               <Body>
                 <Button transparent>
                   <Icon name="chatbubbles" />
-                  <Text>6 attempts blocked</Text>
+                  <Text>16 attempts blocked</Text>
                 </Button>
               </Body>
               <Right>
-                <Text>11h ago</Text>
+                <Text>Points earned: 240</Text>
+              </Right>
+            </CardItem>
+          </Card>
+            <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri: 'https://www.drinkaware.co.uk/media/1058/alcohol-and-bowel-cancer_drinks.jpg?anchor=center&mode=crop&width=721&height=512&rnd=131389799560000000'}} />
+                <Body>
+                  <Text>Alcohol</Text>
+                  <Text note>Beer, wine, rum</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={{uri: 'https://www.eatrightontario.ca/EatRightOntario/media/Website-images-resized/Alcohol-v-2-resized.jpg'}} style={{height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="flame" />
+                  <Text>Block</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon name="chatbubbles" />
+                  <Text>24 attempts blocked</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>Points earned: 2048</Text>
               </Right>
             </CardItem>
           </Card>
@@ -106,11 +137,42 @@ export default class CardImageExample extends Component {
                 </Button>
               </Body>
               <Right>
-                <Text>11h ago</Text>
+                <Text>Points earned: 2048</Text>
               </Right>
             </CardItem>
           </Card>
-        </Content>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri: 'https://genesistransformation.files.wordpress.com/2014/11/coffee.jpg'}} />
+                <Body>
+                  <Text>Dessert</Text>
+                  <Text note>Icecream shops, donut shops, etc</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={{uri: 'http://images.girlslife.com/image.axd?i=91125'}} style={{height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="flame" />
+                  <Text>Block</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon name="chatbubbles" />
+                  <Text>6 attempts blocked</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>Points earned: 634</Text>
+              </Right>
+            </CardItem>
+          </Card>
+                </Content>
       </Container>
     );
   }
